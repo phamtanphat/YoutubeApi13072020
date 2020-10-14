297fe9cd80656a7e459301c44a6c2d19a56affe4
phatdroid94 <phatdroid94@gmail.com>
2020-10-14 19:24:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/youtubeapi13072020/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.youtubeapi13072020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    YouTubePlayerView youTubePlayerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        youTubePlayerView = findViewById(R.id.youtubePlayerView);
+
+        youTubePlayerView.initialize("AIzaSyDgQFlPEnAuZxzaO-Ert2sdBLx3_HOA7VI", new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("Dd_aCizougw");
+                youTubePlayer.setFullscreen(true);
+
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        });
+    }
+}
